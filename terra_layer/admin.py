6df36fcdcf856d9011da430921d4e47372fe865a
Cédric Farcy <cedric.farcy@makina-corpus.com>
2020-11-02 09:48:05 +0100

    Removed unused filter fields from layer viewset

--- terra_layer/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from mapbox_baselayer.admin import MapBaseLayerAdmin
-from mapbox_baselayer.models import MapBaseLayer
-
-admin.site.register(MapBaseLayer, MapBaseLayerAdmin)
