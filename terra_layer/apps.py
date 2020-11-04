0c8aa6ba903911fb89c68d398d400ebdefa28f85
J-E Castagnede <j.e.castagnede@gmail.com>
2020-11-04 11:20:34 +0100

    prefix permissions name to enable module auto discovering

--- terra_layer/apps.py
+++ terra_layer/apps.py
@@ -6,6 +6,6 @@ class TerraLayerConfig(PermissionRegistrationMixin, AppConfig):
     name = "terra_layer"
 
     permissions = (
-        ("can_manage_layers", "Can manage layers"),
-        ("can_manage_sources", "Can manage sources"),
+        ("can_manage_layers", "DataLayer: Can manage layers"),
+        ("can_manage_sources", "DataSource: Can manage sources"),
     )
