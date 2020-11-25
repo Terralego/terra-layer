712f890287cce41088adf6f196ed3fe0b1217bd7
Cédric Farcy <cedric.farcy@makina-corpus.com>
2020-11-25 10:44:24 +0100

    Add reverse data migration

--- terra_layer/migrations/0055_auto_20201124_1036.py
+++ terra_layer/migrations/0055_auto_20201124_1036.py
@@ -16,6 +16,15 @@ def populate_minisheet_config(apps, schema_editor):
         layer.save()
 
 
+def reverse_populate_minisheet_config(apps, schema_editor):
+    LayerModel = apps.get_model("terra_layer", "Layer")
+    for layer in LayerModel.objects.all():
+        layer.minisheet_enable = layer.minisheet_config.get("enable", False)
+        layer.minisheet_template = layer.minisheet_config.get("template")
+        layer.highlight_color = layer.minisheet_config.get("highlight_color")
+        layer.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -24,7 +33,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(
-            populate_minisheet_config,
-            reverse_code=migrations.RunPython.noop,
+            populate_minisheet_config, reverse_code=reverse_populate_minisheet_config
         ),
     ]
