54a0f62ab4a3723b9ec94c41d93fd96092b832a0
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-01-13 15:26:18 +0100

    Add style utils

---
+++ terra_layer/style/__init__.py
@@ -0,0 +1,10 @@
+from terra_layer.style.style import generate_style_from_wizard
+
+from terra_layer.style.utils import (
+    trunc_scale,
+    get_min_max,
+    circle_boundaries_candidate,
+    round_scale,
+    ceil_scale,
+    circle_boundaries_filter_values,
+)
